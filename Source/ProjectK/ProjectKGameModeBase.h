35c0c41b4fb11519d6e9a48e32bfb9808cde1bb8
Umin Lee <netuee@naver.com>
2020-10-26 13:32:57 +0900

    MainProject
    메인 프로젝트 생성

---
+++ Source/ProjectK/ProjectKGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ProjectKGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTK_API AProjectKGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
